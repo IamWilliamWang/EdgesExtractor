75fbcc921aa068892efcadd384e1b98f15173c30
William Wang <iamjerichoholic@hotmail.com>
2019-04-07 14:48:58 +0800

    霍夫变换开发，但无法输出结果

--- OutputStatic.m
+++ OutputStatic.m
@@ -1,7 +1,7 @@
 edgesFrame = getEdgesFromVideo('开关柜_边缘输出.avi');
 [~,~,~,frameCount] = size(edgesFrame);
 for loop=1:4
-    startFrame=frameCount*loop/(loop+1);
+    startFrame=frameCount*loop/(loop+1); %处理从多少帧到结尾
     result=getStaticFrame(edgesFrame,startFrame);
     subplot(2,2,loop)
     imshow(result)
--- getStaticFrame.m
+++ getStaticFrame.m
@@ -1,8 +1,11 @@
-function [result] = getStaticFrame(edgesFrame,startFrame,endFrame)
+function [result] = getStaticFrame(edgesFrame,startFrame,endFrame,step)
 % 由edges四维数组提取出维持不动的像素点
 % 返回[帧宽，帧高，帧深度=1]
-% 输入参数：[edgesFrame]四维图像数组,[startFrame=1]开始帧,[endFrame=帧数]结束帧
+% 输入参数：[edgesFrame]四维图像数组,[startFrame=1]开始帧,[endFrame=帧数]结束帧,[step]循环步伐
     [width,height,depth,frameCount]=size(edgesFrame);
+    if nargin<4
+        step=1;
+    end
     if nargin<3
        endFrame=frameCount;
     end
@@ -12,7 +15,7 @@ function [result] = getStaticFrame(edgesFrame,startFrame,endFrame)
     result = ones(width,height,depth); %创建等同第一帧大小的ones
     startFrame=floor(startFrame); %整形化参数
     endFrame=floor(endFrame);
-    for depth=startFrame:endFrame
+    for depth=startFrame:step:endFrame
         result = result & edgesFrame(:,:,:,depth);
     end
 end
